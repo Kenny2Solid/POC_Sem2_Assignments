82b6ce5be71cc7e00cfc603dcc489e27a3d7389c
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-01-12 09:34:10 -0500

    assignment 1

---
+++ 01_Strings/03_String Comparisons/Assignment.py
@@ -0,0 +1,4 @@
+text1 = input("hello")
+text2 = input("Hello")
+
+print(text1 == text2)
---
+++ 01_Strings/04_Join, Split, and Sort Strings/Assignment.py
@@ -0,0 +1,7 @@
+first_name =  input("what is your first name ?")
+Last_name =   input("what is your Last name ?")
+
+sep = " "
+full_name =sep.join([first_name, Last_name])   #YOUDO use the join function to 
+#join together first_name and last_name 
+print(full_name)
