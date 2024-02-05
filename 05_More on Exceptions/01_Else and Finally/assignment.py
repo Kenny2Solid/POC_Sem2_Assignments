5050097d512afca15f9125346b933c7e3b9c2945
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-02-05 09:22:57 -0500

    dadd

--- 05_More on Exceptions/01_Else and Finally/assignment.py
+++ 05_More on Exceptions/01_Else and Finally/assignment.py
@@ -4,20 +4,22 @@ number1 = 0
 number2 = 0
 try:
     number1 = int(input("Enter a number"))
-    # YOUDO.  use input function and int to set number2
+    number2 = int(input("Enter a number greater then number one"))
 except ValueError:
-    # print message stating that an integer wasn't given
-    pass  # YOUDO remove pass when done
+    print('an integer wwas not given')
+    
 else:
     print("No value error detected")
 finally:
     print("Values taken care of")
 
 try:
-    # YOUDO divide number1 / number2 and set to answer
-    # YOUDO  print the result of the division (aka answer with some helper text)
-    pass  # YOUDO remove pass when done
+    answer = (number1/number2)
+    print("answer equals" , answer)
+ 
 except ZeroDivisionError:
-    # YOUDO:  print message stating that division by zero is not possible.
-    pass  # YOUDO remove pass when done
+    print("Cannot divide by zero")
+else:
+    print('numbers were divible')
+
 #YOUDO:  else and finally here as well.  
