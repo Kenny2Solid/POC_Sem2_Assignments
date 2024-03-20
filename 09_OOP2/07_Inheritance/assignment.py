b646d8c1b904ad8e9a3f9346c7f46d5e76b56ca7
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-03-20 09:25:14 -0400

    ggg

---
+++ 09_OOP2/07_Inheritance/assignment.py
@@ -0,0 +1,36 @@
+class Rectangle:
+
+    def __init__(self, base: float, height: float) -> None:
+        if (base < 0):
+            self.__base = 0
+        else:
+            self.__base = base
+
+        if (height < 0):
+            self.__height = 0
+        else:
+            self.__height = height
+
+    def get_height(self) -> float:
+        return self.__height
+
+    def get_base(self) -> float:
+        return self.__base
+
+    def get_perimeter(self) -> float:
+        return 2 * self.__base + 2 * self.__height
+
+    def get_area(self) -> float:
+        return self.__base * self.__height
+ 
+
+    
+    def __str__(self) -> str:
+        # Rectangle of base:3, height:4
+        return "Rectangle of base:" + self.__base + ", height:" + self.__height
+
+class Square(Rectangle):
+    def __init__(self, side: float) -> None:
+        super().__init__(side, side)
+        self.__side = side
+    def get_side(self)  
