5bdcf76676302df8ac3ad62bb27cd226ec7563d5
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-04-09 09:58:27 -0400

    ggggt

---
+++ 12_GameDesign_1/tic_tac.py
@@ -0,0 +1,10 @@
+grid = [
+    ["-", "-", "-"]
+    ["-", "-", "-"]
+    ["-", "-", "-"]
+]
+
+def print_grid():
+    for row in range(len(grid[row])):
+        for col in range(len(grid[row])):
+            print(grid[row[col]])
